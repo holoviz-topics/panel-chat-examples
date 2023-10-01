fd7074f8331bc2d74040446ff6229f8ff9c9dfe0
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2023-10-01 06:07:52 +0000

    simplify key manager

--- examples/ui_components/key_manager.py
+++ examples/ui_components/key_manager.py
@@ -1,5 +1,5 @@
-"""The `KeyManager` enables you to manage keys from a combination of environment variables.
-and user input.
+"""The `KeyManager` enables you to manage keys from a combination of input arguments,
+environment variables and user input.
 
 For example you might not have the resources to provide an `OpenAI_API_KEY` or `WEAVIATE_API_KEY`.
 Then you would like to ask the user for it.
@@ -13,25 +13,26 @@ WIDGET_MAX_WIDTH = 600
 
 
 class KeyManagerBase(pn.viewable.Viewer):
-    """The `KeyManager` enables you to manage keys from a combination of environment variables.
-and user input.
+    """The `KeyManager` enables you to manage keys from a combination of input arguments,
+    environment variables and user input.
 
-For example you might not have the resources to provide an `OpenAI_API_KEY` or `WEAVIATE_API_KEY`.
-Then you would like to ask the user for it.
+    For example you might not have the resources to provide an `OpenAI_API_KEY` or
+    `WEAVIATE_API_KEY`. Then you would like to ask the user for it.
 
-Inherit from this widget to create you custom `KeyManager`.
+    Inherit from this widget to create your own custom `KeyManager`.
 
-    >>> class KeyManager(KeyManagerBase):
-    ...     OPENAI_API_KEY = param.String(doc="A key for the OpenAI api")
-    ...     WEAVIATE_API_KEY = param.String(doc="A key for the Weaviate api")
-    ...     LANGCHAIN_API_KEY = param.String(doc="A key for the LangChain api")
-"""
+        >>> class KeyManager(KeyManagerBase):
+        ...     OPENAI_API_KEY = param.String(doc="A key for the OpenAI api")
+        ...     WEAVIATE_API_KEY = param.String(doc="A key for the Weaviate api")
+        ...     LANGCHAIN_API_KEY = param.String(doc="A key for the LangChain api")"""
 
     message_alert: str = param.String(
         (
             "**Protect your keys!** If you are using this tool online make sure you trust "
             "the publisher of this app before entering your keys."
-        )
+        ),
+        doc="""An Alert message to display to the user to make them handle their keys securely.
+        If not set, then no Alert is diplays""",
     )
 
     keys_not_set = param.List(constant=True, doc="A list of the keys with no value")
@@ -39,6 +40,9 @@ Inherit from this widget to create you custom `KeyManager`.
 
     def __init__(self, **params):
         self._keys = self._get_keys()
+
+        for key in self._keys:
+            params[key] = params.get(key, os.environ.get(key, ""))
 
         layout_params = {}
         for key, value in params.items():
@@ -49,7 +53,6 @@ Inherit from this widget to create you custom `KeyManager`.
 
         super().__init__(**params)
 
-        self._update_missing_keys(None)
         self._layout = self._create_layout(**layout_params)
 
     def __panel__(self):
@@ -59,6 +62,10 @@ Inherit from this widget to create you custom `KeyManager`.
         return tuple(key for key in self.param if not key in KeyManagerBase.param)
 
     def _create_layout(self, **params):
+        self._update_missing_keys(None)
+        if not self.keys_not_set:
+            return pn.Column(height=0, width=0, margin=0, sizing_mode="fixed")
+
         layout = pn.Column(**params)
         if self.message_alert:
             alert = pn.pane.Alert(
@@ -68,10 +75,7 @@ Inherit from this widget to create you custom `KeyManager`.
             )
             layout.append(alert)
 
-        for key in self._keys:
-            if os.environ.get(key):
-                continue
-
+        for key in self.keys_not_set:
             parameter = self.param[key]
             input_widget = pn.widgets.PasswordInput.from_param(
                 parameter,
@@ -79,30 +83,16 @@ Inherit from this widget to create you custom `KeyManager`.
                 sizing_mode="stretch_width",
                 align="center",
             )
+
             pn.bind(self._update_missing_keys, input_widget, watch=True)
             layout.append(input_widget)
         return layout
-
-    def get_value(self, key):
-        """Returns the value of the specified key
-
-        >>> manager.get_value("OPENAI_API_KEY")
-
-        If the parameter corresponding to the key has a value, it is returned
-        If os.environ has a value corresponding to the key, it is returned
-        Otherwise "" is returned
-        """
-        if key not in self._keys:
-            return ""
-        if getattr(self, key):
-            return getattr(self, key)
-        return os.environ.get(key, "")
 
     def _update_missing_keys(self, _):
         missing = []
         not_missing = []
         for key in self._keys:
-            if not self.get_value(key):
+            if not getattr(self, key):
                 missing.append(key)
             else:
                 not_missing.append(key)
@@ -112,8 +102,10 @@ Inherit from this widget to create you custom `KeyManager`.
 
 
 if pn.state.served:
+
     class KeyManager(KeyManagerBase):
         """An example Key Manager"""
+
         OPENAI_API_KEY = param.String(doc="A key for the OpenAI api")
         WEAVIATE_API_KEY = param.String(doc="A key for the Weaviate api")
         LANGCHAIN_API_KEY = param.String(doc="A key for the LangChain api")
@@ -122,5 +114,11 @@ if pn.state.served:
     pn.template.FastListTemplate(
         title="Key Manager",
         sidebar=[manager],
-        main=[__doc__, pn.Column(manager.param.keys_set, manager.param.keys_not_set)],
+        main=[
+            __doc__,
+            pn.Column(
+                manager.param.keys_set,
+                manager.param.keys_not_set,
+            ),
+        ],
     ).servable()
