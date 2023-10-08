accecc8de05c98c2d798c74ca16deac7abc37e96
ahuang11 <huang.andrew12@gmail.com>
2023-10-08 16:43:28 -0700

    FIx test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,8 +1,6 @@
 """Test all app .py files py running them with python"""
+import subprocess
 
 
 def test_app(app_path):
-    """Test an app. Can you run it with 'python path_to_file.py'?"""
-    with open(app_path, "r", encoding="utf8") as file:
-        code = file.read()
-    exec(code)  # pylint: disable=exec-used
+    subprocess.call(['python', app_path])
