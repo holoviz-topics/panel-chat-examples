a45a6325f7e2af9e9c5b55671d73d0e938a6f219
Andrew Huang <ahuang@anaconda.com>
2023-10-11 11:58:37 -0700

    Add template

--- docs/examples/mistral/chat.py
+++ docs/examples/mistral/chat.py
@@ -4,22 +4,34 @@ Mistral thru CTransformers.
 """
 
 import panel as pn
-from ctransformers import AutoModelForCausalLM
+from ctransformers import AutoConfig, AutoModelForCausalLM, Config
 
 pn.extension()
+
+INSTRUCTIONS = "You are a friendly chat bot willing to help out the user."
+
+
+def apply_template(instructions, contents):
+    text_row = f"""<s>[INST]{instructions} {contents}[/INST]"""
+    return text_row
 
 
 async def callback(contents: str, user: str, instance: pn.widgets.ChatInterface):
     if "mistral" not in llms:
         instance.placeholder_text = "Downloading model; please wait..."
+        config = AutoConfig(
+            config=Config(
+                temperature=0.5, max_new_tokens=2048, context_length=2048, gpu_layers=1
+            ),
+        )
         llms["mistral"] = AutoModelForCausalLM.from_pretrained(
             "TheBloke/Mistral-7B-Instruct-v0.1-GGUF",
             model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-            gpu_layers=1,
+            config=config,
         )
 
     llm = llms["mistral"]
-    response = llm(contents, stream=True, max_new_tokens=1000)
+    response = llm(apply_template(INSTRUCTIONS, contents), stream=True)
     message = ""
     for token in response:
         message += token
@@ -27,7 +39,12 @@ async def callback(contents: str, user: str, instance: pn.widgets.ChatInterface)
 
 
 llms = {}
-chat_interface = pn.widgets.ChatInterface(callback=callback, callback_user="Mistral")
+chat_interface = pn.widgets.ChatInterface(
+    callback=callback,
+    callback_user="Mistral",
+    reset_on_send=True,
+    widgets=[pn.widgets.TextAreaInput(auto_grow=True, rows=1, max_rows=3)],
+)
 chat_interface.send(
     "Send a message to get a reply from Mistral!", user="System", respond=False
 )
