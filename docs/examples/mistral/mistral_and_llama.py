11cc4afc7c87c608175eda2e046fc76f5dc88a29
Andrew Huang <ahuang@anaconda.com>
2023-10-17 08:47:08 -0700

    Add mistral and llama

---
+++ docs/examples/mistral/mistral_and_llama.py
@@ -0,0 +1,46 @@
+"""
+Demonstrates how to use the ChatInterface widget to create a chatbot using
+Llama2 and Mistral.
+"""
+
+import panel as pn
+from ctransformers import AutoModelForCausalLM
+
+pn.extension()
+
+MODEL_ARGUMENTS = {
+    "llama": {
+        "args": ["TheBloke/Llama-2-7b-Chat-GGUF"],
+        "kwargs": {"model_file": "llama-2-7b-chat.Q5_K_M.gguf"},
+    },
+    "mistral": {
+        "args": ["TheBloke/Mistral-7B-Instruct-v0.1-GGUF"],
+        "kwargs": {"model_file": "mistral-7b-instruct-v0.1.Q4_K_M.gguf"},
+    },
+}
+
+
+def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
+    for model in MODEL_ARGUMENTS:
+        if model not in pn.state.cache:
+            pn.state.cache[model] = AutoModelForCausalLM.from_pretrained(
+                *MODEL_ARGUMENTS[model]["args"],
+                **MODEL_ARGUMENTS[model]["kwargs"],
+                gpu_layers=1,
+            )
+
+        llm = pn.state.cache[model]
+        response = llm(contents, max_new_tokens=512, stream=True)
+
+        entry = None
+        for chunk in response:
+            entry = instance.stream(chunk, user=model.title(), entry=entry)
+
+
+chat_interface = pn.chat.ChatInterface(callback=callback)
+chat_interface.send(
+    "Send a message to get a reply from both Llama 2 and Mistral (7B)!",
+    user="System",
+    respond=False,
+)
+chat_interface.servable()
