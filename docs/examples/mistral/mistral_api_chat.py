07e0bb121eed8f915e6b4d522f4afcc8903ed631
ergodiclife <58880657+ergodiclife@users.noreply.github.com>
2023-12-17 22:21:30 +0000

    Create mistral_api_chat.py

---
+++ docs/examples/mistral/mistral_api_chat.py
@@ -0,0 +1,28 @@
+import panel as pn
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+pn.extension()
+
+
+async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
+    messages = []
+    messages.append(ChatMessage(role="user", content=contents))
+
+    mistral_response = ""
+    for chunk in client.chat_stream(model="mistral-tiny", messages=messages):
+        response = chunk.choices[0].delta.content
+        if response is not None:
+            mistral_response += response
+            yield mistral_response
+
+    if mistral_response:
+        messages.append(ChatMessage(role="assistant", content=mistral_response))
+
+
+client = MistralClient()  # api_key=os.environ.get("MISTRAL_API_KEY", None),
+chat_interface = pn.chat.ChatInterface(callback=callback, callback_user="Mistral AI")
+chat_interface.send(
+    "Send a message to get a reply from Mistral AI!", user="System", respond=False
+)
+chat_interface.servable()
