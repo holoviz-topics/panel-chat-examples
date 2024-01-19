a045834d8676dbcba7907367337713b896206c0a
Andrew Huang <ahuang@anaconda.com>
2024-01-19 13:06:58 -0800

    Spacing

--- docs/examples/langchain/langchain_lcel.py
+++ docs/examples/langchain/langchain_lcel.py
@@ -22,8 +22,8 @@ async def callback(contents, user, instance):
 prompt = ChatPromptTemplate.from_template("Tell me a top-notch joke about {topic}")
 model = ChatOpenAI(model="gpt-3.5-turbo")
 output_parser = StrOutputParser()
-
 chain = {"topic": RunnablePassthrough()} | prompt | model | output_parser
+
 chat_interface = pn.chat.ChatInterface(
     pn.chat.ChatMessage(
         "Offer a topic and ChatGPT will respond with a joke!", user="System"
