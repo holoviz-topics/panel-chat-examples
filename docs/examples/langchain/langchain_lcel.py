9b57f6e8adebefcf9e4fdd4dff362fa4061b0ca6
Andrew Huang <ahuang@anaconda.com>
2024-01-24 20:36:49 -0800

    Add memory

--- docs/examples/langchain/langchain_lcel.py
+++ docs/examples/langchain/langchain_lcel.py
@@ -3,30 +3,51 @@ Demonstrates how to use the `ChatInterface` to create a chatbot using
 [LangChain Expression Language](https://python.langchain.com/docs/expression_language/) (LCEL).
 """
 
+from operator import itemgetter
+
 import panel as pn
+from langchain.memory import ConversationBufferMemory
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_openai import ChatOpenAI
 
 pn.extension()
 
+SYSTEM_PROMPT = "Try to be a silly comedian."
+
 
 async def callback(contents, user, instance):
     message = ""
-    async for token in chain.astream(contents):
+    inputs = {"input": contents}
+    async for token in chain.astream(inputs):
         message += token
         yield message
+    memory.save_context(inputs, {"output": message})
 
 
-prompt = ChatPromptTemplate.from_template("Tell me a top-notch joke about {topic}")
 model = ChatOpenAI(model="gpt-3.5-turbo")
+memory = ConversationBufferMemory(return_messages=True)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", SYSTEM_PROMPT),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{input}"),
+    ]
+)
 output_parser = StrOutputParser()
-chain = {"topic": RunnablePassthrough()} | prompt | model | output_parser
+chain = (
+    RunnablePassthrough.assign(
+        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+    )
+    | prompt
+    | model
+    | output_parser
+)
 
 chat_interface = pn.chat.ChatInterface(
     pn.chat.ChatMessage(
-        "Offer a topic and ChatGPT will respond with a joke!", user="System"
+        "Offer a topic and ChatGPT will try to be funny!", user="System"
     ),
     callback=callback,
     callback_user="ChatGPT",
